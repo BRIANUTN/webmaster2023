99361357751bb03576aea55d1f15a42b4c6cb4f9
brian pedrosa <brianemanuelpedrosa@hotmail.com>
2023-07-31 13:25:34 -0300

    correccion

--- M5 RUTAS/routes/galeria.js
+++ M5 RUTAS/routes/galeria.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', function(req, res, next){
-    res.render('galeria') //view/nosotros.hbs
+    res.render('galeria') //view/galeria.hbs
 })
 
 module.exports = router
--- M5 RUTAS/routes/novedades.js
+++ M5 RUTAS/routes/novedades.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', function(req, res, next){
-    res.render('novedades') //view/nosotros.hbs
+    res.render('novedades') //view/novedades.hbs
 })
 
 module.exports = router
