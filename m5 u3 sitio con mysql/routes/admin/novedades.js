b3607632a8c6f0f7677df61f9186cc4040512934
brian pedrosa <brianemanuelpedrosa@hotmail.com>
2023-09-26 12:23:40 -0300

    agregar servicios form

--- m5 u3 sitio con mysql/routes/admin/novedades.js
+++ m5 u3 sitio con mysql/routes/admin/novedades.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModels');
 
-router.get('/', function (req, res, next){
+router.get('/', async function (req, res, next){
+
+    var novedades = await novedadesModel.getNovedades();
+
     res.render('admin/novedades', {
         layout: 'admin/layout',
-        usuario: req.session.nombre
+        usuario: req.session.nombre,
+        novedades
+    });
+});
+
+
+router.get('/eliminar/:id', async (req, res, next) => {
+    var id = req.params.id;
+    await novedadesModel.deleteNovedadesById(id);
+    res.redirect('/admin/novedades');
+});
+
+router.get('/agregar', (req, res, next) => {
+    res.render('admin/agregar', {
+        layout: 'admin/layout'
     });
 });
 
--- m5 u3 sitio con mysql/routes/servicios.js
+++ m5 u3 sitio con mysql/routes/servicios.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+
+  
   res.render('servicios',{
     isServicios:true
   });
